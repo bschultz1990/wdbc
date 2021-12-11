ffb16cce934ac13ed132b50103be6e4a3c38e5fe
bschultz1990 <bschultz1990@users.noreply.github.com>
2021-12-10 21:21:16 -0600

    colorPicker.css colorPicker.html colorPicker.js app.js index.html

---
+++ Projects/QuerySelector Exercise/app.js
@@ -0,0 +1,5 @@
+// select the <span> element that reads "delicious."
+// Change the text to read "Disgusting" using JS.
+
+const taste = document.querySelector("h1 span")
+taste.innerHTML = "Disgusting"
