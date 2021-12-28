ca7c62f98c57656e8296f6188e2121ed4170eb1f
Ben <bschultz1990@gmail.com>
2021-12-27 20:09:24 -0600

    _

---
+++ Projects/buttonExercise/app.js
@@ -0,0 +1,10 @@
+// for (let i = 0; i < 100; i++) {
+//     let newButton = document.createElement('button');
+//     newButton.innerText = 'HEY! LISTEN!';
+//     Document.querySelector('#container').appendChild(newButton);
+// }
+
+
+let newImg = document.createElement('img'); // Create image
+newImg.src = 'https://d.furaffinity.net/art/jackrow/1319519761/1319519761.jackrow_frisky_corvids_1200x895.png'; // Assign source to image
+document.querySelector('#container').append(newImg); // Add image to container.
