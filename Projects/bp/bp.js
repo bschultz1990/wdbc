af98a3a3208b57159009af145101472727f3d859
Ben Schultz <ben@schultz.com>
2022-10-26 17:00:11 -0500

    Added files via CMD

--- Projects/bp/bp.js
+++ Projects/bp/bp.js
@@ -1,8 +1,10 @@
 const fs = require('fs'); // Import the fs module
-const contents = process.argv[1] // path to the JS file bein executed
-const htmlbp = fs.readFileSync(`${contents}/index.html`)
+const Path = require ('path');
 
-console.log(htmlbp)
+// path to the JS file being executed
+// regex here is lookahead assertion. Match anything before bp.js or bp.
+const directory = process.argv[1].match(/.*(?=bp.js|bp)/);
+
 // Store your own argument as a variable.
 const folderName = process.argv[2] || 'NewFolder'
 
@@ -10,7 +12,9 @@ const folderName = process.argv[2] || 'NewFolder'
 fs.mkdirSync(folderName);
 console.log(`Created project folder: ${folderName}`);
 
+const htmlbp = fs.readFileSync(`${directory}/sources/index.html`, 'utf8');
+
 // Make folder contents inside new folder:
-fs.writeFileSync(`${folderName}/index.html`, ``);
+fs.writeFileSync(`${folderName}/index.html`, htmlbp);
 fs.writeFileSync(`${folderName}/styles.css`, '');
 fs.writeFileSync(`${folderName}/app.js`, '');
