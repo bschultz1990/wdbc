1cf27eda96c0c9860682e199e65bdb85952edc22
Ben Schultz <ben@schultz.com>
2022-11-29 19:09:19 -0600

    Node JS project.

---
+++ Projects/2022-11-29_Language_Guesser/index.js
@@ -0,0 +1,6 @@
+import {franc} from 'franc'
+// const langs = require('langs');
+
+// let langCode = franc('Senatus Populusque Romanus');
+// let language = langs.where("3", langCode);
+// console.log(langCode)
