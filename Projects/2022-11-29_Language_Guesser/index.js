6141c538ecca6005166eb9de22484ed1bc545db2
Ben Schultz <ben@schultz.com>
2022-11-30 16:58:17 -0600

    Added files via CMD

--- Projects/2022-11-29_Language_Guesser/index.js
+++ Projects/2022-11-29_Language_Guesser/index.js
@@ -1,6 +1,16 @@
+// Use Import and Require in the same file:
+import {createRequire} from "module";
+const require = createRequire(import.meta.url);
 import {franc} from 'franc'
-// const langs = require('langs');
+const langs = require('langs');
 
-// let langCode = franc('Senatus Populusque Romanus');
-// let language = langs.where("3", langCode);
-// console.log(langCode)
+let langCode = franc(process.argv[2])
+let language = langs.where("3", langCode);
+// if (language === "und"){
+// 	console.log("More info needed. Try a larger sample.")
+// }
+// else {
+// 	console.log(`Language guess: ${language.name}`);
+// }
+
+console.log(language.name)
